d7585a52c4ae7cceaa4870befac655fa8ea33bc3
ellis <mapakameellis@gmail.com>
2021-06-28 20:44:31 +0700

    third commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName ='BOT ==> ';
+const botName ='BOT (0 i 0)';
 
 //run when client connects
 io.on('connection',socket => {
